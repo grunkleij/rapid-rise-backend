821988c6939e8c359fe1d866e5f5424d33f3b24f
grunkleij <indrajithpg10@gmail.com>
2026-03-02 20:21:35 +0530

    bug password on windows fixed

--- src/app.module.ts
+++ src/app.module.ts
@@ -17,6 +17,7 @@ import { FilesModule } from './files/files.module';
       host: process.env.DB_HOST,
       port: parseInt(process.env.DB_PORT ?? '5432'),
       username: process.env.DB_USERNAME,
+      password: String(process.env.DB_PASSWORD),
       database: process.env.DB_DATABASE,
       autoLoadEntities: true,
       synchronize: true,
